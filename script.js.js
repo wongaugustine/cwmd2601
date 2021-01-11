e0957c57a10dd2dffd505b6de5ed8dffd4456542
wongaugustine <72580849+wongaugustine@users.noreply.github.com>
2021-01-11 11:42:56 -0500

    Add files via upload

---
+++ script.js.js
@@ -0,0 +1,13 @@
+// IC2 Purpose: student to fill in
+
+document.getElementById('process').onclick = function (){
+    
+    // declare variables and initialize them with DOM input
+    let myInput = document.getElementById('firstinput').value;
+
+    // DOM output with string expressions
+    document.getElementById('firstoutput').innerHTML = `Input value: ${myInput}.`;
+
+};
+
+
